e67ab3bf4910d8c334ae1d40112d548219b1b927
Bofferbrauer <75318450+Bofferbrauer@users.noreply.github.com>
2021-03-12 12:26:12 +0100

    script

--- Scripts/dataEN.js
+++ Scripts/dataEN.js
@@ -1,57 +1,27 @@
 const articlesArray = [
     // { template:
-    //     Group: "",
-    //     Title: "", 
-    //     Paragraph1: "",
-    //     Paragraph2: "",
-    //     Paragraph3: "",
+    //     group: "",
+    //     title: "", 
+    //     paragraph1: "",
+    //     paragraph2: "",
+    //     paragraph3: "",
     // }
 
         {
-        Group: "Introduction",
-        Title: "Introduction",
-        Paragraph1: "Hello everybody to the Weblink repository of the My Compass, CodeStart and CodingJobs formations by NumericAll. This is a collection of all the links to the different tools that are being used during the trainings, of which there are A LOT.",
-        Paragraph2: "So, why make this website? Well, because during the My Compass training, we used a whole new array of tools and programs practically each and every new day. In the end, I counted over 30 programs and websites that we used during the course. With so many tools and utilities, it becomes hard to keep them all in mind and a good overview over all of them. So, with the suggestion of my fellow former My Compass alumni, this website is meant to give everybody a good overview over all the tools that are used throughout the course, and for the other trainings, give a glimpse into all the tools that are used these days by web developers, be they specialized in the Front-end, Back-end, SEO or UX/UI. This is why I'm making this website",
-        Paragraph3: "",
-    }
+        group: "Introduction",
+        title: "Introduction",
+        paragraph1: "Hello everybody to the Weblink repository of the My Compass, CodeStart and CodingJobs formations by NumericAll. This is a collection of all the links to the different tools that are being used during the trainings, of which there are A LOT.",
+        paragraph2: "So, why make this website? Well, because during the My Compass training, we used a whole new array of tools and programs practically each and every new day. In the end, I counted over 30 programs and websites that we used during the course. With so many tools and utilities, it becomes hard to keep them all in mind and a good overview over all of them. So, with the suggestion of my fellow former My Compass alumni, this website is meant to give everybody a good overview over all the tools that are used throughout the course, and for the other trainings, give a glimpse into all the tools that are used these days by web developers, be they specialized in the Front-end, Back-end, SEO or UX/UI. This is why I'm making this website",
+        paragraph3: "",
+    },
 
          {
-        Group: "Introduction",
-        Title: "The List of tools",
-        Paragraph1: "All the tools that we used will be listed here, sorted by category. Some may be downloadable, which will be mentioned if this is the case, but most of them are used online.",
-        Paragraph2: "Keep in mind we only ever used free or time-limited versions of the programs. So if you never used any of them before, they should all be available for free, even though for some of them just for a free introductionary week or month.",
-        Paragraph3: "So, without further ado, let's get started!",
+        group: "Introduction",
+        title: "The List of tools",
+        paragraph1: "All the tools that we used will be listed here, sorted by category. Some may be downloadable, which will be mentioned if this is the case, but most of them are used online.",
+        paragraph2: "Keep in mind we only ever used free or time-limited versions of the programs. So if you never used any of them before, they should all be available for free, even though for some of them just for a free introductionary week or month.",
+        paragraph3: "So, without further ado, let's get started!",
     }
     
-        {
-        Group: "",
-        Title: "",
-        Paragraph1: "",
-        Paragraph2: "",
-        Paragraph3: "",
-    }
 
-        {
-        Group: "",
-        Title: "",
-        Paragraph1: "",
-        Paragraph2: "",
-        Paragraph3: "",
-    }
-
-        {
-        Group: "",
-        Title: "",
-        Paragraph1: "",
-        Paragraph2: "",
-        Paragraph3: "",
-    }
-
-        {
-        Group: "",
-        Title: "",
-        Paragraph1: "",
-        Paragraph2: "",
-        Paragraph3: "",
-    }
 ]
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,30 @@
 for (let i = 100; i >= 0; i -= 10) {
     console.log(i)
 }
+
+constapp = Vue.createApp({
+    data() {
+        return {
+            title: "Title",
+            paragraph1: "Paragraphe 1",
+            paragraph2: "Paragraphe 2",
+            paragraph3: "Paragraphe 3",
+
+        };
+    },
+
+    methods: {
+        async getArticleData() {
+
+            const res = await fetch("dataEN.js");
+            const jsonRes = await res.json();
+            const myArticle = jsonRes.results;
+
+            this.title = myArticle.title;
+            this.paragraph1 = myArticle.paragraph1;
+            this.paragraph2 = myArticle.paragraph2;
+            this.paragraph3 = myArticle.paragraph3;
+        }
+    }
+});
+applicationCache.mount("#mainapp")
